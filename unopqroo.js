e68ef6c23fe1583418543ba6f01613f3450a316c
Pedro1462 <pedrojosueriverocanche@gmail.com>
2023-10-10 21:04:51 -0500

    Repartir Cartas

--- unopqroo.js
+++ unopqroo.js
@@ -1,5 +1,6 @@
 
 //regresa el valor de una variable
+const { log } = require("console");
 const rl = require("readline-sync");
 
 // juego uno
@@ -11,42 +12,71 @@ var type = ["Comun", "Especial", "Comodin"];
 //carta comun, cartas comodin, numeros
 // atributo o caracteristicas es lo que define un objeto
 
-var card ={
+var card = {
     color: null, // amarillo, azul, verde y rojo
     number: null, //0 al 9
-    type:null, // comun, especial y comodin
+    type: null, // comun, especial y comodin
 };
 
-for (var cColor = 0; cColor < colors.length; cColor++){
-    for(var cNumber = 0; cNumber < 10; cNumber++){   
-        deck.push({color: colors[cColor], number: cNumber, type: "Comun"});
-    }   
-    for(var cNumber1 = 1; cNumber1 < 10; cNumber1++){
-        deck.push({color: colors[cColor], number: cNumber1, type: "Comun"});        
+for (var cColor = 0; cColor < colors.length; cColor++) {
+    for (var cNumber = 0; cNumber < 10; cNumber++) {
+        deck.push({ color: colors[cColor], number: cNumber, type: "Comun" });
+    }
+    for (var cNumber1 = 1; cNumber1 < 10; cNumber1++) {
+        deck.push({ color: colors[cColor], number: cNumber1, type: "Comun" });
     }
 
-    deck.push({color: colors[cColor], number: "+2", type: "Especial"});
-    deck.push({color: colors[cColor], number: "+2", type: "Especial"});
-    deck.push({color: colors[cColor], number: "B", type: "Especial"});
-    deck.push({color: colors[cColor], number: "B", type: "Especial"});
-    deck.push({color: colors[cColor], number: "R", type: "Especial"});
-    deck.push({color: colors[cColor], number: "R", type: "Especial"});
+    deck.push({ color: colors[cColor], number: "+2", type: "Especial" });
+    deck.push({ color: colors[cColor], number: "+2", type: "Especial" });
+    deck.push({ color: colors[cColor], number: "B", type: "Especial" });
+    deck.push({ color: colors[cColor], number: "B", type: "Especial" });
+    deck.push({ color: colors[cColor], number: "R", type: "Especial" });
+    deck.push({ color: colors[cColor], number: "R", type: "Especial" });
 
-    deck.push({color: null, number: "+4", type: "Comodin"});
-    deck.push({color: null, number: "+4", type: "Comodin"});
-    deck.push({color: null, number: "+4", type: "Comodin"});
-    deck.push({color: null, number: "+4", type: "Comodin"});
-    deck.push({color: null, number: "CC", type: "Comodin"});
-    deck.push({color: null, number: "CC", type: "Comodin"});
-    deck.push({color: null, number: "CC", type: "Comodin"});
-    deck.push({color: null, number: "CC", type: "Comodin"});
+    deck.push({ color: null, number: "+4", type: "Comodin" });
+    deck.push({ color: null, number: "+4", type: "Comodin" });
+    deck.push({ color: null, number: "+4", type: "Comodin" });
+    deck.push({ color: null, number: "+4", type: "Comodin" });
+    deck.push({ color: null, number: "CC", type: "Comodin" });
+    deck.push({ color: null, number: "CC", type: "Comodin" });
+    deck.push({ color: null, number: "CC", type: "Comodin" });
+    deck.push({ color: null, number: "CC", type: "Comodin" });
 }
 
 //console.log(JSON.stringify(deck));
 
 const players = rl.question("Cuantos jugadores seran?", {})
 
-console.log(players);
+//console.log(players);
 
 //init sirve para inicializar un proyecto de node
 //npm i o npm install <modulo> sirve para instalar el modulo en el proyecto
+
+
+//Dependencia: un codigo que depende de otro codigo
+//Paquete aquello que guarda informacions
+
+//Repartir 7 cartas por jugador
+//Recorro el total de tarjetas (total de tarjetas = tarjetas/7 * jugador)
+//Un arreglo por jugador
+const totalCards = players * 7;
+
+var cardsPlayers = {};
+
+for (var cTCards = 0; cTCards < 7; cTCards++) {
+    for (var cPlayers = 0; cPlayers < players; cPlayers++) {
+        //console.log("player: ", cPlayers);
+        //con ! si no existe
+        if (!cardsPlayers["player_"+cPlayers]) {
+            cardsPlayers["player_"+cPlayers] = [];    
+        }
+        
+        cardsPlayers["player_"+cPlayers].push(deck[cTCards]);
+    }
+}
+
+console.log(cardsPlayers);
+
+//investigar comparadores en node para que sirve, para que funciona y un ejemplo
+//investigar validadores de variables
+//
